c742116c38a01ed9d34858f440ffb3b3a911be4b
Rolando Santos <rsantos2032@gmail.com>
2025-02-15 18:02:58 -0600

    Added client code

---
+++ client/src/App.tsx
@@ -0,0 +1,33 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './assets/App.css';
+import Main from './pages/Main';
+
+function App() {
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.tsx</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Main />}></Route>
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
---
+++ client/src/pages/Main.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import '../assets/Main.css';
+
+const Main: React.FC = () => {
+  return (
+    <div className="main">
+      <h1>Poem Library</h1>
+      
+      <div>
+        <h2>Young Cardinals</h2>
+        Red birds bleed and blue ones die. That's the nature of this world of mine.
+        <br/>
+        No Song to sing nor hymn to cry. Just an aching heart and watery eyes.
+        <br/>
+        I hate love and lust and God and lies. I hate hummingbirds. The word "goodbye".
+        <br/>
+        But if you ask me now, beneath this sky.
+        <br/>
+        I can tell you why, the young cardinals fly.
+      </div>
+    </div>
+  );
+};
+
+export default Main;
